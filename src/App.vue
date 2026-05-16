ee6c6cffba9c5295f8c6c56ff1db137eff4aaeec
Taufiq Kurniawan Akbar <taufiq.ttgi@gmail.com>
2026-05-16 14:37:29 +0700

    install package nuxt ui

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
-</template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-950">
+    <UCard class="w-96">
+      <template #header>
+        <h1 class="text-xl font-bold">
+          Dashboard
+        </h1>
+      </template>
+
+      <div class="space-y-4">
+        <UInput placeholder="Masukkan nama..." />
+
+        <UButton block>
+          Submit
+        </UButton>
+      </div>
+    </UCard>
+  </div>
+</template> 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
-import router from './router'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import ui from '@nuxt/ui/vue-plugin'
+
+const app = createApp(App)
+
+app.use(ui)
+
+app.mount('#app')
